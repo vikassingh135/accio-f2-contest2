542fdc9059088fec7af31940788ec3981bb867a4
vikassingh135 <vikassingh1352003@gmail.com>
2023-05-13 17:52:07 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(student => {
+      if(student.marks>50) console.log(student);
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if(student.marks>50) console.log(student);
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i].marks<50) {
+      arr.splice(i,1);
+    }
+  }
+
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 5, name: "alice", age: "16", marks: 70 },
+    { id: 6, name: "bob", age: "25", marks: 90 },
+    { id: 7, name: "shan", age: "30", marks: 30 },
+  ];
+
+  let concatenated_arr = arr.concat(arr2);
+  console.log(concatenated_arr);
 }
+
+
+PrintStudentswithMap();
+PrintStudentsbyForEach();
+addData();
+removeFailedStudent();
+concatenateArray();
